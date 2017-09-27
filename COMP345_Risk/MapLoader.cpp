e37d8d7b32300fa85658ec8c302bfd4a81688feb
Brandon Huang <brandonbgda@hotmail.com>
2017-09-27 00:00:41 -0400

    tentative finalized maploader
    Still needs to instantiate a Map object and verify against invalid Map objects

--- COMP345_Risk/MapLoader.cpp
+++ COMP345_Risk/MapLoader.cpp
@@ -29,14 +29,23 @@ MapLoader::MapLoader(std::string fileDirectory) {
 					territory.setCountryName(lineVector.at(0));
 					territory.setX(stoi(lineVector.at(1)));
 					territory.setY(stoi(lineVector.at(2)));
+					territory.setContinent(lineVector.at(3));
 					vector<Country*> neighborCountries;
-					for (int i = 3; i < lineVector.size(); i++) {
+					for (int i = 4; i < lineVector.size(); i++) {
 						Country* country = new Country(lineVector[i]);
 						territory.setNeighboringCountries(country);
 					}
 				
-				cout << "Country name is:" << territory.getCountryName() << endl;
-				cout << "X:" << territory.getX() << " Y:" << territory.getY() << endl;
+				cout << "Country name is: " << territory.getCountryName() << endl;
+				cout << "X: " << territory.getX() << " Y: " << territory.getY() << endl;
+				cout << "Continent: " << territory.getContinent() << endl;
+				cout << "Neighbor countries: ";
+				vector<Country*> vectorNeighbor = territory.getNeighboringCountries();
+				for (int j = 0; j<vectorNeighbor.size(); j++) {
+					cout << vectorNeighbor[j]->getCountryName() << " ";
+				}
+				cout << endl << endl;
+
 			}
 		}
 		if (line.find("[Territories]") != std::string::npos) {
