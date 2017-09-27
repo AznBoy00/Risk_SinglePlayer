8dba236eae100a72ace31ccf11d45b0f61657978
Shawn Guerra-Bautitsta <ShawnGuerraBautista@gmail.com>
2017-09-27 12:00:34 -0400

    Improved MapLoader and commented

--- COMP345_Risk/MapLoader.cpp
+++ COMP345_Risk/MapLoader.cpp
@@ -17,32 +17,37 @@ MapLoader::MapLoader(std::string fileDirectory) {
 	while (std::getline(inputfilestream, line)) {
 		if (recordingTerritories) {
 			if (!line.empty()) {
-				Country territory;
+				Country* territory = new Country();
 				listOfTerritories.push_back(line);
 				stringstream ss(line);
+				std::string word;
 				vector<string> lineVector;
-				while (ss) {
-					string substring;
-					getline(ss, substring, ',');
-					lineVector.push_back(substring);
+
+				//Improved while loop
+				//B4 it actually added an additional empty element
+				while (std::getline(ss, word, ',')) {
+					lineVector.push_back(word);
 				}
-					territory.setCountryName(lineVector.at(0));
-					territory.setX(stoi(lineVector.at(1)));
-					territory.setY(stoi(lineVector.at(2)));
-					territory.setContinent(lineVector.at(3));
+					territory->setCountryName(lineVector.at(0));
+					territory->setX(stoi(lineVector.at(1)));
+					territory->setY(stoi(lineVector.at(2)));
+					territory->setContinent(lineVector.at(3));
 					vector<Country*> neighborCountries;
 					for (int i = 4; i < lineVector.size(); i++) {
+						//Change this loop and try to implement the Map and Continent classes
+						//The issue is that you keep on creating multiple country objects even if they exist
+						//(i.e. If you have two country whose neighbor is Alaska; you are essentailly creating 2 Alakas)
 						Country* country = new Country(lineVector[i]);
-						territory.setNeighboringCountries(country);
+						territory->setNeighboringCountries(country);
 					}
 				
-				cout << "Country name is: " << territory.getCountryName() << endl;
-				cout << "X: " << territory.getX() << " Y: " << territory.getY() << endl;
-				cout << "Continent: " << territory.getContinent() << endl;
+				cout << "Country name is: " << territory->getCountryName() << endl;
+				cout << "X: " << territory->getX() << " Y: " << territory->getY() << endl;
+				cout << "Continent: " << territory->getContinent() << endl;
 				cout << "Neighbor countries: ";
-				vector<Country*> vectorNeighbor = territory.getNeighboringCountries();
+				vector<Country*> vectorNeighbor = territory->getNeighboringCountries();
 				for (int j = 0; j<vectorNeighbor.size(); j++) {
-					cout << vectorNeighbor[j]->getCountryName() << " ";
+					cout << vectorNeighbor[j]->getCountryName() << ", ";
 				}
 				cout << endl << endl;
 
