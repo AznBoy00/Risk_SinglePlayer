b002c148d5dc34a4700d2956b8b63fd96a87aa17
blazeincursio <matthewto1@hotmail.com>
2017-09-28 00:10:03 -0400

    Add files via upload
    updated files. still missing the driver. which is complete bullshit.

---
+++ deck.h
@@ -0,0 +1,17 @@
+using namespace std;
+
+class deck {
+public: 
+	
+
+	string card(string country, string type);
+	string getType();
+	string print();
+	deck::deck();
+	string hand();
+	string draw();
+	string exchange();
+	void printdeck();
+	void shuffle();
+	string driver();
+};
