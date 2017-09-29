bea839eb6c5945edbbb1c71c80db475d2c0bff7c
Kevin <Lin_Kevin_1995@hotmail.com>
2017-09-29 11:26:30 -0400

    Cleanup Deck, revamp soon, working and compilable.

--- COMP345_Risk/main.cpp
+++ COMP345_Risk/main.cpp
@@ -1,5 +1,4 @@
 #include "Dice.h"
-#include "Deck.h"
 #include "Player.h"
 #include "Time.h"
 #include "main.h"
@@ -8,65 +7,54 @@
 #include <iostream>
 using namespace std;
 
-/*
-	Main driver method.
-*/
 int main() {
 	loadMenu();
 	return 0;
 }
-/*
-	Test menu for assignment 1.
-*/
+
 void loadMenu() {
 	int selection;
 	cout << "Welcome to Team18's COMP345 Risk Game!" << endl;
-	
+
 	do {
 		cout << "\n\nAssignment 1 Debug Menu" << endl;
-		cout << "1- Read Map" << endl;
-		cout << "2- Load Map" << endl;
-		cout << "3- Roll Dice" << endl;
-		cout << "4- Show player object/Action options" << endl;
-		cout << "5- Card/Deck methods" << endl;
-		cout << "6- Exit" << endl;
+		cout << "1- Load Map" << endl;
+		cout << "2- Roll Dice" << endl;
+		cout << "3- Show player object/Action options" << endl;
+		cout << "4- Card/Deck methods" << endl;
+		cout << "5- Exit" << endl;
 
 		cin >> selection;
 		switch (selection) {
-		case 1://Read map
-			readMaps();
-			break;
-		case 2://Load map
+		case 1://Load map
 			loadMaps();
 			break;
-		case 3://Roll Dice + Create players
+		case 2://Roll Dice + Create players
 			loadDice();
 			break;
-		case 4://Show player actions
+		case 3://Show player actions
 			playerActions();
 			break;
-		case 5://Card/Deck method showoffs
+		case 4://Card/Deck method showoffs
 			cardDeckMethods();
 			break;
-		case 6://Exit
+		case 5://Exit
 			exit(0);
 			break;
 		default:
 			break;
 		}
-	} while (selection != 6);
+	} while (selection != 5);
 }
-/*
-	Load maps from map.cpp
-*/
+
+void readMaps() {
+}
+
 void loadMaps() {
-	cout << "\n(Part 1 & 2)Map demo:" << endl;
 	MapLoader("World.map");
 	//system("pause");
 }
-/*
-	Demonstrates the dice engine of the game.
-*/
+
 void loadDice() {
 	srand(time(0));
 	int num;
@@ -100,101 +88,9 @@ void loadDice() {
 	p2->getDice().showStats();
 	//p2->showStats();
 }
-/*
-	Shows a sample of the turn player's menu.
-*/
+
 void playerActions() {
-	int selection;
-	Player *p1 = new Player(1);
-	
-	cout << "\n(Part 4)Player(1) action menu demo:" << endl;
-	cout << "Turn player action:" << endl;
-	cout << "1-Show player info" << endl;
-	cout << "2-Reinforce" << endl;
-	cout << "3-Attack" << endl;
-	cout << "4-Fortify" << endl;
-	cin >> selection;
-
-	switch (selection) {
-		case 1:
-			p1->showStats();
-			break;
-		case 2:
-			p1->reinforce();
-			break;
-		case 3:
-			p1->attack();
-			break;
-		case 4:
-			p1->fortify();
-			break;
-		default:
-			break;
-	}
 }
-/*
-	Create a deck object, then cards will be drawn and shown until the deck decks out.
-*/
+
 void cardDeckMethods() {
-	Deck newDeck = Deck();
-	cout << "Deck created." << endl;
-	newDeck.shuffle();
-	cout << "Deck shuffled." << endl;
-
-	Deck infantryArray[14];
-	Deck artilleryArray[14];
-	Deck cavalryArray[14];
-	int infantrycounter, artillerycounter, cavalrycounter;
-
-	for (int i = 0; i < 42; i++) {// loop to identify each card type from the deck
-								 // and place them in a array of that said type
-		if (newDeck[i].getType().compare("Infantry") == 0) {
-			infantrycounter = 0;
-			infantryArray[infantrycounter] = newDeck[i];
-			infantrycounter++;
-		}
-
-		if (newDeck[i].getType().compare("Artillery") == 0) {
-			artillerycounter = 0;
-			artilleryArray[artillerycounter] = newDeck[i];
-			artillerycounter++;
-		}
-
-		if (newDeck[i].getType().compare("Cavalry") == 0) {
-			cavalrycounter = 0;
-			cavalryArray[cavalrycounter] = newDeck[i];
-			cavalrycounter++;
-		}
-
-	}
-
-	for (int i = 0; i < 14; i++)
-		infantryArray[i].print();
-	cout << infantrycounter + " infantry cards" << endl;
-
-	for (int i = 0; i < 14; i++)
-		artilleryArray[i].print();
-	cout << artillerycounter + " artillery cards" << endl;
-
-	for (int i = 0; i < 14; i++)
-		cavalryArray[i].print();
-	cout << infantrycounter + " cavalry cards" << endl;
-
-	Hand newHand = Hand();// create hand. 
-	int play;
-	cout << "Show me your moves." << endl;
-	cout << " Enter 1 to draw or 2 to exchange hand and 3 to quit." << endl;
-
-	cin >> play;
-	if (play == 1) {
-	newHand.draw();
-	play = 0;
-}
-if (play == 2) {
-	newHand.exchange();
-	play = 0;
-}
-else exit(0);
-
-
 }
